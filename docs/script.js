ba019b058db95151319c5d70ec37744b8e5887b0
dgl-11 <dgl-11@users.noreply.github.com>
2026-08-27 00:47:51 +0300

    RELEASE 1.1.0

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,5 @@
 (() => {
-  const APP_VERSION = "1.0.5";
+  const APP_VERSION = "1.1.0";
 
   // Update dynamic version links and text elements
   document.querySelectorAll("[data-version-href]").forEach((el) => {
